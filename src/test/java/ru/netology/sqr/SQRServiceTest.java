bbd8a18d3af76f838dfc3c761b42e2918633fcdd
kaninaoksana <oksana_1710@mail.ru>
2022-06-30 23:47:09 +0300

    1

---
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -0,0 +1,19 @@
+package ru.netology.sqr;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.testng.annotations.Test;
+
+public class SQRServiceTest {
+
+
+    @Test
+    public void shouldCalc() {
+        SQRService service = new SQRService();
+        int actual = service.calculate(200, 300);
+        int expected = 3;
+
+
+        assertEquals(expected , actual);
+    }
+}
